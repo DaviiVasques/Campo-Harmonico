e396b94703de39bc357647e7f766fbcddf2d530e
DaviiVasques <davivasques07@gmail.com>
2023-11-27 18:20:59 -0300

    commit inicial

---
+++ CampoHarmonico.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+
+class CampoHarmonico
+{
+    public List<string> notasValidas = new List<string> { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+
+    public bool ValidarNota(string nota)
+    {
+        return notasValidas.Contains(nota);
+    }
+
+    public List<string> ObterCampoHarmonico(string nota)
+    {
+        List<string> notas = new List<string>
+        {
+            "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"
+        };
+
+        int indiceNota = notas.IndexOf(nota);
+
+        if (indiceNota == -1)
+        {
+            Console.WriteLine("Nota inválida.");
+            return new List<string>();
+        }
+
+        List<string> campoHarmonico = new List<string>
+        {
+            notas[indiceNota], notas[(indiceNota + 2) % 12], notas[(indiceNota + 4) % 12],
+            notas[(indiceNota + 5) % 12], notas[(indiceNota + 7) % 12], notas[(indiceNota + 9) % 12],
+            notas[(indiceNota + 11) % 12]
+        };
+
+        return campoHarmonico;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite qual campo harmônico deseja saber: ");
+        string tom = Console.ReadLine().ToUpper();
+
+        CampoHarmonico CampHarm = new CampoHarmonico();
+
+        if (CampHarm.ValidarNota(tom))
+        {
+            List<string> campoHarmonicoDo = CampHarm.ObterCampoHarmonico(tom);
+
+            Console.WriteLine($"Campo Harmônico de {tom}:");
+
+            foreach (var nota in campoHarmonicoDo)
+            {
+                Console.Write($"{nota} ");
+            }
+
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine("Nota inválida.");
+        }
+    }
+}
